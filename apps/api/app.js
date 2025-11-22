2afe3183058ae36413c1db58ad6970d00d40f7cd
chacka1315 <chacka131@gmail.com>
2025-11-22 12:41:50 +0000

    FInish  implementating first version of the api.

--- apps/api/app.js
+++ apps/api/app.js
@@ -1,28 +1,45 @@
+import 'dotenv/config';
 import express from 'express';
 import morgan from 'morgan';
-import 'dotenv/config';
-import { fileURLToPath } from 'node:url';
-import path from 'node:path';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+import { NotFoundError } from './errors/CustomErrors.js';
+import routes from './routes/index.js';
+import auth from './middlewares/auth.js';
 
 const app = express();
+
+const blog = express.Router();
+const blogAdmin = express.Router();
 
 //globals middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 
-//Errors handling
-app.use((req, resm, next) => {
-  next(new Error('Page not found!'));
+app.use('/api/admin', auth.checkRole('ADMIN'), blogAdmin);
+app.use('/api', blog);
+
+// Blog routing
+blog.use('/posts', routes.post);
+blog.use('/comments', routes.comment);
+blog.use('/auth', routes.auth);
+blog.use('/users', routes.user);
+
+//Blog routing for admins
+blogAdmin.use('/posts', routes.postAdmin);
+blogAdmin.use('/users', routes.userAdmin);
+
+app.use((req, res) => {
+  const error = new NotFoundError('This page does not exist.');
+  res.status(error.statusCode).json({
+    msg: error.message,
+  });
 });
 
+//Errors handling
 app.use((err, req, res, next) => {
   console.error(err);
   res.status(err.statusCode || 500).json({
-    message: err.message,
+    msg: 'Server is broken, try later...',
   });
 });
 
